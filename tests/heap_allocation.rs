cb88474ba56dbf749c881e6b4e37ac9ddf635e9b
Xiangyi Meng <xymeng16@gmail.com>
2021-09-03 01:20:27 +0800

    Add test for heap allocation.

---
+++ tests/heap_allocation.rs
@@ -0,0 +1,77 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+use bootloader::boot_info::FrameBuffer;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use rust_os::allocator::HEAP_SIZE;
+
+entry_point!(ktest_main);
+
+fn ktest_main(boot_info: &'static mut BootInfo) -> ! {
+    use rust_os::allocator;
+    use rust_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
+
+    if let Some(fb) = boot_info.framebuffer.as_mut() {
+        init(fb);
+    }
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset.into_option().unwrap());
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_regions)
+    };
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialization failed");
+
+    test_main();
+    loop {}
+}
+
+pub fn init(fb: &'static mut FrameBuffer) {
+    let fb_info = fb.info();
+
+    unsafe { rust_os::vga_buffer::init_global_writer(fb.buffer_mut(), fb_info); }
+
+    rust_os::init();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn simple_allocation() {
+    let heap_value_1 = Box::new(41);
+    let heap_value_2 = Box::new(13);
+    assert_eq!(*heap_value_1, 41);
+    assert_eq!(*heap_value_2, 13);
+}
+
+#[test_case]
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
